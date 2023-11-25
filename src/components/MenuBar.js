3585be86682cd39d2b1e62c60160ebff60d2182e
Diani Dickovita <108466170+DianiDickovita@users.noreply.github.com>
2023-11-25 12:55:55 +0530

    diani's_first_commit

---
+++ src/components/MenuBar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './menubar.css'
+
+export default function MenuBar() {
+  return (
+    <div>
+        <div className="menubar">
+            <div className="menubar-name">
+                Diani Dickovita
+            </div>
+            <div className="menubar-tabs">
+                <a href='#'>Home</a>
+                <a href='#'>About</a>
+                <a href='#'>Experience</a>
+                <a href='#'>Project</a>
+                <a href='#'>Contact</a>
+
+            </div>
+
+        </div>
+    </div>
+  )
+}
